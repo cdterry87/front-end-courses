4a560f50371d7441ef7345e1a98559912bb4c57b
Chase Terry <chase.terry87@gmail.com>
2020-09-29 23:04:01 -0500

    Started working with props, state, and events in React

--- react-schwarzmuller/react-complete-guide/src/App.js
+++ react-schwarzmuller/react-complete-guide/src/App.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Person from './Person/Person'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      {
+        name: 'Chase',
+        age: 33
+      },
+      {
+        name: 'Danielle',
+        age: 28
+      },
+      {
+        name: 'Caiden',
+        age: 4
+      }
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log('on click')
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World</h1>
+        <button onClick={ this.switchNameHandler }>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My hobbies: coding</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: art</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
+      </div>
+    )
+  }
 }
 
 export default App;
