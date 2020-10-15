af130856c08f842f02ea5d4667a69d3a2ca1947c
Chase Terry <chase.terry87@gmail.com>
2020-10-15 12:22:26 -0500

    Added syncing across tabs with window storage listener

--- js-mead/notes-app/edit.js
+++ js-mead/notes-app/edit.js
@@ -1,12 +1,11 @@
 const id = location.hash.substring(1)
-const notes = getSavedNotes()
 
 const titleElement = document.querySelector('#note-title')
 const bodyElement = document.querySelector('#note-body')
 const removeButton = document.querySelector('#remove-note')
 
-
-const note = notes.find((note) => {
+let notes = getSavedNotes()
+let note = notes.find((note) => {
   return note.id === id
 })
 
@@ -29,4 +28,18 @@ removeButton.addEventListener('click', (e) => {
   removeNote(id)
   saveNotes(notes)
   location.assign('/index.html')
+})
+
+window.addEventListener('storage', function (e) {
+  if (e.key === 'notes') {
+    notes = JSON.parse(e.newValue)
+    note = notes.find((note) => {
+      return note.id === id
+    })
+
+    if (!note) location.assign('/index.html')
+
+    titleElement.value = note.title
+    bodyElement.value = note.body
+  }
 })
