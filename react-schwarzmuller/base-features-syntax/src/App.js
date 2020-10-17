06a23db480910b9292d7b58d3eb2d4d269acca4f
Chase Terry <chase.terry87@gmail.com>
2020-10-17 11:06:55 -0500

    Working with lists and conditionals in react

--- react-schwarzmuller/base-features-syntax/src/App.js
+++ react-schwarzmuller/base-features-syntax/src/App.js
@@ -5,9 +5,9 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stephanie', age: 26 }
+      { id: 1, name: 'Max', age: 28 },
+      { id: 2, name: 'Manu', age: 29 },
+      { id: 3, name: 'Stephanie', age: 26 }
     ],
     otherState: 'some other value',
     showPersons: false
@@ -25,18 +25,35 @@ class App extends Component {
     } )
   }
 
-  nameChangedHandler = (event) => {
-    this.setState( {
-      persons: [
-        { name: 'Max', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 26 }
-      ]
-    } )
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(person => {
+      return person.id === id
+    })
+
+    // Objects are reference types so we want to create a copy of the person in the object rather than mutate persob object in state directly
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+
+    person.name = event.target.value
+
+    const persons = [...this.state.persons]
+    persons[personIndex] = person
+
+    console.log(persons)
+
+    this.setState({persons:persons})
   }
 
   togglePersonsHandler = () => {
     this.setState({showPersons: !this.state.showPersons})
+  }
+
+  deletePersonHandler = (index) => {
+    // const persons = this.state.persons.slice() // make a copy of the original array
+    const persons = [...this.state.persons] // make a copy of the original array
+    persons.splice(index, 1)
+    this.setState({persons: persons})
   }
 
   render () {
@@ -48,6 +65,24 @@ class App extends Component {
       cursor: 'pointer'
     }
 
+    let persons = null
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person
+              key={person.id}
+              name={person.name}
+              age={person.age}
+              click={() => this.deletePersonHandler(index)} 
+              changed={(event) => this.nameChangedHandler(event, person.id)}
+            />
+          })}
+        </div>
+      )
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
@@ -56,21 +91,7 @@ class App extends Component {
           style={style}
           onClick={this.togglePersonsHandler}
         >Toggle Persons</button>
-        { this.state.showPersons ?
-          <div>
-            <Person 
-              name={this.state.persons[0].name} 
-              age={this.state.persons[0].age} />
-            <Person 
-              name={this.state.persons[1].name} 
-              age={this.state.persons[1].age}
-              click={this.switchNameHandler.bind(this, 'Max!')}
-              changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
-            <Person 
-              name={this.state.persons[2].name} 
-              age={this.state.persons[2].age} />
-          </div> : null
-        }
+        {persons}
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
