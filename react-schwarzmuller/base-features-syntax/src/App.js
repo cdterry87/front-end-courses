8f5fad33d7171ca35ba6ee0d89f6b4823058f176
Chase Terry <chase.terry87@gmail.com>
2020-10-17 20:11:54 -0500

    Switched to CSS modules in React app

--- react-schwarzmuller/base-features-syntax/src/App.js
+++ react-schwarzmuller/base-features-syntax/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 
@@ -58,9 +58,7 @@ class App extends Component {
   }
 
   render () {
-    const style = {
-      
-    }
+    let btnClass = [classes.Button]
 
     let persons = null
 
@@ -78,27 +76,31 @@ class App extends Component {
           })}
         </div>
       )
+
+      btnClass.push(classes.Red)
     }
 
-    let classes = []
+    let assignedClasses = []
     if (this.state.persons.length <= 2) {
-      classes.push('red')
+      assignedClasses.push(classes.red)
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold')
+      assignedClasses.push(classes.bold)
     }
     
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
+        <p className={assignedClasses.join(' ')}>This is really working!</p>
         <button
-          style={style}
+          className={btnClass.join(' ')}
           onClick={this.togglePersonsHandler}
         >Toggle Persons</button>
         {persons}
       </div>
     );
+
+    // This is what the above code will compile to
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
