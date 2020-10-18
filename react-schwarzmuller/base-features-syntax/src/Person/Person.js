4e221bda185d7eaf51e4d7f30adb9a93c0241acc
Chase Terry <chase.terry87@gmail.com>
2020-10-18 18:04:51 -0500

    Debugging

--- react-schwarzmuller/base-features-syntax/src/Person/Person.js
+++ react-schwarzmuller/base-features-syntax/src/Person/Person.js
@@ -2,6 +2,11 @@ import React from 'react';
 import classes from './Person.css';
 
 const person = ( props ) => {
+    const rnd = Math.random() 
+    console.log('rnd', rnd)
+    if (rnd >= 0.7) {
+        throw new Error('Sorry, something went wrong.')
+    }
     return (
         <div className={classes.Person}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
