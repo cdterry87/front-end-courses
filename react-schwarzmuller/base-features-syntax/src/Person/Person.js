ce5c137262ae457ea5fe83993f3778fdae8446ec
Chase Terry <chase.terry87@gmail.com>
2020-10-17 19:56:16 -0500

    About to eject.

--- react-schwarzmuller/base-features-syntax/src/Person/Person.js
+++ react-schwarzmuller/base-features-syntax/src/Person/Person.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import styled from 'styled-components';
 import './Person.css';
-
-const StyledDiv = styled.div`
-    width: 60%;
-    margin: auto;
-    border: 1px solid #eee;
-    box-shadow: 0 2px 3px #ccc;
-    padding: 16px;
-    text-align: center;
-    margin-bottom: 16px;
-`;
 
 const person = ( props ) => {
     return (
-        <StyledDiv>
+        <div>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
-        </StyledDiv>
+        </div>
     )
 };
 
