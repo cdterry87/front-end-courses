e72d2398b840ca952d380cba7eee462286621cf3
Chase Terry <chase.terry87@gmail.com>
2020-11-07 22:14:53 -0600

    Working on React lifecycle hooks

--- react-schwarzmuller/base-features-syntax/src/containers/App.js
+++ react-schwarzmuller/base-features-syntax/src/containers/App.js
@@ -4,6 +4,11 @@ import Persons from '../components/Persons/Persons'
 import Cockpit from '../components/Cockpit/Cockpit'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    console.log('App.js constructor')
+  }
+
   state = {
     persons: [
       { id: 1, name: 'Max', age: 28 },
@@ -12,6 +17,25 @@ class App extends Component {
     ],
     otherState: 'some other value',
     showPersons: false
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('App.js getDerivedStateFromProps', props)
+
+    return state
+  }
+
+  componentDidMount() {
+    console.log('App.js componentDidMount')
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('App.js shouldComponentUpdate')
+    return true
+  }
+
+  componentDidUpdate() {
+    console.log('App.js componentDidUpdate')
   }
 
   switchNameHandler = (newName) => {
@@ -56,6 +80,7 @@ class App extends Component {
   }
 
   render () {
+    console.log('App.js render')
     let persons = null
 
     if (this.state.showPersons) {
@@ -71,6 +96,7 @@ class App extends Component {
     return (
       <div className={classes.App}>
         <Cockpit 
+          title={this.props.appTitle}
           persons={this.state.persons}
           showPersons={this.state.showPersons}
           clicked={this.togglePersonsHandler}
