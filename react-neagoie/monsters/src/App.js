5701d0e73cac92a6f128e81c774d3c0835e684b2
Chase Terry <chase.terry87@gmail.com>
2020-12-05 16:55:22 -0600

    Changed Search Box to component

--- react-neagoie/monsters/src/App.js
+++ react-neagoie/monsters/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { CardList } from './components/CardList/CardList'
+import { SearchBox } from './components/SearchBox/SearchBox'
 
 class App extends Component {
   constructor() {
@@ -17,6 +18,10 @@ class App extends Component {
       .then(users => this.setState({ monsters: users }))
   }
 
+  handleChange = (e) => {
+    this.setState({searchField: e.target.value})
+  }
+
   render() {
     const { monsters, searchField } = this.state
     const filteredMonsters = monsters.filter(monster => 
@@ -25,10 +30,9 @@ class App extends Component {
 
     return (
       <div className="App">
-        <input
-          type="search"
+        <SearchBox
           placeholder="Search monsters"
-          onChange={e => this.setState({ searchField: e.target.value }) }
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters} />
       </div>
