3c4b5df44684b516ddb31e01fc6c445b44d4ab56
Chase Terry <chase.terry87@gmail.com>
2020-12-06 15:22:43 -0600

    Created home page, menu, and menu item components

---
+++ react-neagoie/shop/src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.scss'
+
+import Menu from '../../components/Menu/Menu'
+
+const Home = () => (
+  <div className="home">
+    <div className="menu">
+      <Menu />
+    </div>
+  </div>
+)
+
+export default Home
