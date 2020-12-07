401740bb888c2b0a35ec3f7c005fe90f1417894b
Chase Terry <chase.terry87@gmail.com>
2020-12-06 23:07:52 -0600

    Added a sample hats page route

--- react-neagoie/shop/src/App.js
+++ react-neagoie/shop/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home/Home'
+
+const HatsPage = () => (
+  <h1>Hats Page</h1>
+)
 
 function App() {
   return (
     <div>
-      <Home />
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/shop/hats" component={HatsPage}/>
+      </Switch>
     </div>
   )
 }
