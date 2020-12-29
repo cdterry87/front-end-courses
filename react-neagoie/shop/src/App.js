5ed8f638236548dcee496f386c44399eef3c1198
Chase Terry <chase.terry87@gmail.com>
2020-12-29 14:22:43 -0600

    Added checkout page, checkout item component, and updated cart to use selectors from reselect

--- react-neagoie/shop/src/App.js
+++ react-neagoie/shop/src/App.js
@@ -1,22 +1,27 @@
-import React from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
-import Home from './pages/Home/Home';
-import Shop from './pages/Shop/Shop';
-import Header from './components/Header/Header';
-import SignInSignUp from './pages/SignInSignUp/SignInSignUp';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-import { setCurrentUser } from './redux/user/user.actions';
-import './App.css';
+import React from 'react'
+import { Route, Switch, Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
+
+import Home from './pages/Home/Home'
+import Shop from './pages/Shop/Shop'
+import Header from './components/Header/Header'
+import SignInSignUp from './pages/SignInSignUp/SignInSignUp'
+import Checkout from './pages/Checkout/Checkout'
+
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'
+import { setCurrentUser } from './redux/user/user.actions'
+import { selectCurrentUser } from './redux/user/user.selectors'
+import './App.css'
 
 class App extends React.Component {
-  unsubscribeFromAuth = null;
+  unsubscribeFromAuth = null
 
   componentDidMount() {
-    const { setCurrentUser } = this.props;
+    const { setCurrentUser } = this.props
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
-        const userRef = await createUserProfileDocument(userAuth);
+        const userRef = await createUserProfileDocument(userAuth)
 
         userRef.onSnapshot((snapshot) => {
           setCurrentUser({
@@ -24,16 +29,16 @@ class App extends React.Component {
               id: snapshot.id,
               ...snapshot.data(),
             },
-          });
-        });
+          })
+        })
       } else {
-        setCurrentUser(userAuth);
+        setCurrentUser(userAuth)
       }
-    });
+    })
   }
 
   componentWillUnmount() {
-    this.unsubscribeFromAuth();
+    this.unsubscribeFromAuth()
   }
 
   render() {
@@ -43,6 +48,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/shop" component={Shop} />
+          <Route exact path="/checkout" component={Checkout} />
           <Route
             exact
             path="/login"
@@ -53,16 +59,16 @@ class App extends React.Component {
           />
         </Switch>
       </div>
-    );
+    )
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser,
-});
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
+})
 
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
-});
+})
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App)
--- react-neagoie/shop/src/components/Header/Header.jsx
+++ react-neagoie/shop/src/components/Header/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
 import { auth } from '../../firebase/firebase.utils'
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import CartIcon from '../CartIcon/CartIcon'
 import CartDropdown from '../CartDropdown/CartDropdown'
+import { selectCartHidden } from '../../redux/cart/cart.selectors'
+import { selectCurrentUser } from '../../redux/user/user.selectors'
 import './Header.scss'
 
 const Header = ({ currentUser, hidden }) => (
@@ -28,9 +31,15 @@ const Header = ({ currentUser, hidden }) => (
   </div>
 )
 
-const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({
-  currentUser,
-  hidden
+// const mapStateToProps = (state) => ({
+//   currentUser: selectCurrentUser(state),
+//   hidden: selectCartHidden(state)
+// })
+
+// This is the same as the commented out section above using createStructuredSelector to get the state and pass it into the selectors below
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+  hidden: selectCartHidden
 })
 
 export default connect(mapStateToProps)(Header)
