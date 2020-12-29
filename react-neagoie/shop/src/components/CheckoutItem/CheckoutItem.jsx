825eeec4f69baaabae258d38b379849b695a6549
Chase Terry <chase.terry87@gmail.com>
2020-12-29 15:00:03 -0600

    Added addItem and subtractItem functionality to checkout item component

--- react-neagoie/shop/src/components/CheckoutItem/CheckoutItem.jsx
+++ react-neagoie/shop/src/components/CheckoutItem/CheckoutItem.jsx
@@ -1,17 +1,40 @@
 import React from 'react'
+import { connect } from 'react-redux'
+
+import { addItem, removeItem, subtractItem } from '../../redux/cart/cart.actions'
 
 import './CheckoutItem.scss'
 
-const CheckoutItem = ({cartItem: {imageUrl, name, quantity, price}}) => (
-  <div className="checkout-item">
-    <div className="image-container">
-      <img src={imageUrl} alt="item"/>
-    </div>
-    <span className="name">{name}</span>
-    <span className="quantity">{quantity}</span>
-    <span className="price">{price}</span>
-    <div className="remove-button">&#10005;</div>
-  </div>
-)
+const CheckoutItem = ({ cartItem, removeItem, addItem, subtractItem }) => {
+  const { imageUrl, name, quantity, price } = cartItem
 
-export default CheckoutItem
+  return (
+    <div className="checkout-item">
+      <div className="image-container">
+        <img src={imageUrl} alt="item"/>
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={() => subtractItem(cartItem)}>
+          &#10094;
+        </div>
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={() => addItem(cartItem)}>
+          &#10095;
+        </div>
+      </span>
+      <span className="price">{price}</span>
+      <div className="remove-button" onClick={() => removeItem(cartItem)}>
+        &#10005;
+      </div>
+    </div>
+  )
+}
+
+const mapDispatchToProps = dispatch => ({
+  removeItem: item => dispatch(removeItem(item)),
+  addItem: item => dispatch(addItem(item)),
+  subtractItem: item => dispatch(subtractItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CheckoutItem)
--- react-neagoie/shop/src/redux/cart/cart.selectors.js
+++ react-neagoie/shop/src/redux/cart/cart.selectors.js
@@ -17,7 +17,6 @@ export const selectCartItemsCount = createSelector(
     )
 )
 
-
 export const selectCartHidden = createSelector(
   [selectCart],
   cart => cart.hidden
