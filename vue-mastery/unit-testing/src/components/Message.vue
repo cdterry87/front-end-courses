e922271eff9b5897187525763261aa3806e2ecc6
Chase Terry <chase.terry87@gmail.com>
2021-01-03 01:23:35 -0600

    Added a message component with test. Added axios and flushPromises and setup an axios service to run the test.

---
+++ vue-mastery/unit-testing/src/components/Message.vue
@@ -0,0 +1,24 @@
+<template>
+  <p v-if="error" data-test-id="error">{{ error }}</p>
+  <p v-else data-test-id="message">{{ message.text }}</p>
+</template>
+
+<script>
+import { getMessage } from '@/services/axios'
+export default {
+  name: 'Message',
+  data() {
+    return {
+      message: {},
+      error: null
+    }
+  },
+  async created() {
+    try {
+      this.message = await getMessage()
+    } catch (err) {
+      this.error = 'Oops! Something went wrong.'
+    }
+  }
+}
+</script>
