275be5afe0ebed5b66af4145222521ec8f9a5ad7
Chase Terry <chase.terry87@gmail.com>
2021-01-03 21:34:02 -0600

    Added MessageContainer component and added tests to handle stubbing the Message child component.

---
+++ vue-mastery/unit-testing/src/components/MessageContainer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Message />
+  </div>
+</template>
+
+<script>
+import Message from '@/components/Message'
+export default {
+  name: 'MessageContainer',
+  components: {
+    Message
+  }
+}
+</script>
