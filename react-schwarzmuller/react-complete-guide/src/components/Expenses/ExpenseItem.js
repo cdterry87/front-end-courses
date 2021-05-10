941ac055a5461bf58274782af2579b9abfd9e955
Chase Terry <chase.terry87@gmail.com>
2021-05-09 21:43:27 -0500

    Finished section 4 of react project. Ready for section 5.

--- react-schwarzmuller/react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-schwarzmuller/react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,24 +1,15 @@
-import { useState } from 'react'
-
 import Card from '../UI/Card'
 import ExpenseDate from './ExpenseDate'
 import './ExpenseItem.css'
 
 const ExpenseItem = ({ title, amount, date }) => {
-  const [expenseTitle, setTitle] = useState(title)
-
-  const clickHandler = () => {
-    setTitle('Updated')
-  }
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   )
 }
--- react-schwarzmuller/react-complete-guide/src/components/Expenses/Expenses.js
+++ react-schwarzmuller/react-complete-guide/src/components/Expenses/Expenses.js
@@ -1,21 +1,33 @@
+import { useState } from 'react'
+
 import Card from '../UI/Card'
 import ExpenseItem from './ExpenseItem'
+import ExpensesFilter from './ExpensesFilter'
 import './Expenses.css'
 
 const Expenses = ({ expenses }) => {
+  const [filteredYear, setFilteredYear] = useState('2021')
+
+  const filterChangedHandler = selectedYear => {
+    setFilteredYear(selectedYear)
+  }
+
   return (
-    <Card className="expenses">
-      {
-        expenses.map(expense => (
-          <ExpenseItem
-            key={expense.title}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))
-      }
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter onChangeFilter={filterChangedHandler} value={filteredYear} />
+        {
+          expenses.map(expense => (
+            <ExpenseItem
+              key={expense.title}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))
+        }
+      </Card>
+    </div>
   )
 }
 
